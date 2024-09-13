0cff57ba6c25a6bec9ffd6eea73dfc205991fb24
Centurionactionx <joseph789222@gmail.com>
2024-09-13 13:10:35 -0400

    Update Payroll_Calculator.java

--- Assignments/Payroll_Calculator.java
+++ Assignments/Payroll_Calculator.java
@@ -48,13 +48,15 @@ public class Payroll_Calculator {
         monthly_pay3 = 4 * rate3 * hours3;
     }
 
-    public void display() {
-        System.out.printf("%70s", "Sein");
-        System.out.printf("%n%n%s %20s %25s %20s", "Employee Name", "Hourly Rate", "Total Hours per Week",
-                "Gross Monthly Pay");
-        System.out.printf("%n%12s %16s%.2f %18.1f %18s%.2f", employee1, "$", rate1, hours1, "$", monthly_pay1);
-        System.out.printf("%n%12s %16s%.2f %18.1f %18s%.2f", employee2, "$", rate2, hours2, "$", monthly_pay2);
-        System.out.printf("%n%12s %16s%.2f %18.1f %18s%.2f", employee3, "$", rate3, hours3, "$", monthly_pay3);
+    public void display_headings(){ // Prints out the table's title and headings
+        System.out.printf("%70s", "Sein Co.");
+        System.out.printf("%n%n%s %20s %25s %20s", "Employee Name", "Hourly Rate", "Total Hours per Week", "Gross Monthly Pay");
+    }
+
+    public void display_info() { // Prints out the information in a table
+        System.out.printf("%n%-24s %s%.2f %18.1f %18s%.2f", employee1, "$", rate1, hours1, "$", monthly_pay1);
+        System.out.printf("%n%-24s %s%.2f %18.1f %18s%.2f", employee2, "$", rate2, hours2, "$", monthly_pay2);
+        System.out.printf("%n%-24s %s%.2f %18.1f %18s%.2f", employee3, "$", rate3, hours3, "$", monthly_pay3);
     }
 
     public static void main(String[] args) {
