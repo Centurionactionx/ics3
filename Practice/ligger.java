d3d605def14f28cca1bc33e5c8c1725800c73d97
Centurionactionx <joseph789222@gmail.com>
2024-11-12 19:42:01 -0500

    balls

---
+++ Practice/ligger.java
@@ -0,0 +1,14 @@
+import java.lang.Object.*;
+
+public class ligger {
+
+    Console c;
+    public void main(){
+        System.out.println("");
+    }
+
+    public static void main(String[] args) {
+        ligger l = new ligger();
+        l.main();
+    }
+}
